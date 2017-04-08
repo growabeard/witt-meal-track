75bdf9e53d5def92f3116f5abe6d491498860409
growabeard <chris.w.witt@gmail.com>
2017-04-07 23:35:02 -0400

    maybe this will fix the sql query..

--- src/main/java/com/witt/tracker/repositories/ProgressRepository.java
+++ src/main/java/com/witt/tracker/repositories/ProgressRepository.java
@@ -2,6 +2,7 @@ package com.witt.tracker.repositories;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.witt.tracker.entities.Progress;
 
@@ -14,7 +15,7 @@ public interface ProgressRepository extends CrudRepository<Progress, Integer> {
 	
 	Progress save(Progress entity);
 
-	@Query("select * from public.progress where updated = ?1")
-	Iterable<Progress> getMatchingDate(String date);
+	@Query("select * from public.progress p where p.updated = date")
+	Iterable<Progress> getMatchingDate(@Param("date") String date);
 	
 }
